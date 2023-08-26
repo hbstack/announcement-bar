581fc1e996f3f3df1321ba60cbbd0b91c306c474
razonyang <razonyang@gmail.com>
2023-08-27 00:14:04 +0800

    fix: make sure clearing previous timer when creating a new timer

--- assets/hb/modules/announcement-bar/js/index.ts
+++ assets/hb/modules/announcement-bar/js/index.ts
@@ -14,6 +14,7 @@ import * as params from '@params'
     }
 
     const setTimer = (): void => {
+      clearTimer()
       let i = 0
       timer = setInterval(() => {
         announcements[i].classList.add('inactivating')
